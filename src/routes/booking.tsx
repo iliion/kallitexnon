3ddf0b5a6a13040e3ff5365e4b0a510dc0f96414
iliion <1ll1as10ann0u@gmail.com>
2026-09-21 12:51:37 +0300

    Change CALENDLY_URL to new form link
    Updated CALENDLY_URL to a new Google Forms link.

--- src/routes/booking.tsx
+++ src/routes/booking.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-const CALENDLY_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeoienCt9OkrunqhU79ZjY2wRFeX2Hu-3zZbEl2bzG6NwOUHA/viewform?embedded=true"; // TODO: replace with real link
+const CALENDLY_URL = "https://docs.google.com/forms/d/e/1FAIpQLSd_V9NxTCfhu0uBML1BmWQ0oY1ssnU9_pGL0ZLDP0IxpzZ4gQ/viewform?usp=publish-editor?embedded=true"; // TODO: replace with real link
 
 export const Route = createFileRoute("/booking")({
   head: () => ({
